faa98add8fb0c38e2bfc664c75162006271aa200
Nolan Lawson <nolan.lawson@gmail.com>
2016-06-06 17:20:43 -0700

    make this into a preset

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-<<<<<<< 77c7206e993c1ac95cf54546aadb488402777e8d
-memory-adapter
-====================
-
-In-memory adapter for PouchDB
-=======
 pouchdb-memory
 =======
 
@@ -17,4 +11,3 @@ This build combines the following plugins:
 - `pouchdb-find`
 
 It should be useful for things like quick unit tests. Feel free to add additional plugins on top of it, or just build your own – the source code is tiny!
->>>>>>> initial commit
--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 'use strict';
 
-<<<<<<< 77c7206e993c1ac95cf54546aadb488402777e8d
-var leveldown = require('memdown');
-leveldown.adapterName = 'memory';
-leveldown.valid = function () {
-  return true;
-}
-leveldown.usePrefix = false;
-
-module.exports = leveldown;
-=======
 module.exports = require('pouchdb-core')
   .plugin(require('pouchdb-adapter-memory'))
   .plugin(require('pouchdb-mapreduce'))
   .plugin(require('pouchdb-replication'))
   .plugin(require('pouchdb-find'));
->>>>>>> initial commit
