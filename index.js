e09a18326075f2c8e81da9eecbae23a8c74a202c
Nolan Lawson <nolan.lawson@gmail.com>
2016-06-28 22:10:35 -0700

    add pouchdb-adapter-http

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = require('pouchdb-core')
   .plugin(require('pouchdb-adapter-memory'))
+  .plugin(require('pouchdb-adapter-http'))
   .plugin(require('pouchdb-mapreduce'))
   .plugin(require('pouchdb-replication'))
   .plugin(require('pouchdb-find'));
